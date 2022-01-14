39d52db37be97787454f4501212662adfd3cfd1a
Max <maxsevostianov@192.168.1.3>
2022-01-14 14:25:59 -0500

    created partial for 404 page

--- wp-content/themes/regulora/resources/views/404.blade.php
+++ wp-content/themes/regulora/resources/views/404.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  @if ( ! have_posts() )
-    @if ( $notFoundPage = \App\Controllers\App::themeOptions( '404page' ) )
-      {!! apply_filters( 'the_content', $notFoundPage->post_content ) !!}
-    @else
-      <h2 class="alert alert-warning mb-5">
-          NO RESULTS FOUND
-      </h2>
-      <div>The page you requested could not be found. Try refining your search.</div>
-    @endif
-  @endif
+  @include('partials.404-title')
 @endsection
---
+++ wp-content/themes/regulora/resources/views/partials/404-title.blade.php
@@ -0,0 +1,12 @@
+<div class="alignfull top-info">
+    <div class="container-fluid container-xl">
+      <div class="row align-items-center">
+        <div class="name col-12">
+          <h2 class="alert alert-warning mb-5">
+          NO RESULTS FOUND
+      </h2>
+      <div>The page you requested could not be found. Try refining your search.</div>
+        </div>
+      </div>
+    </div>
+</div>
