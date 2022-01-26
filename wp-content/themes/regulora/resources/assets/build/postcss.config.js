e200e4656524cbd37845cff096a7f7d3b5f6e93e
Max <maxsevostianov@192.168.1.3>
2022-01-26 12:02:10 -0500

    compiler test #4

--- wp-content/themes/regulora/resources/assets/build/postcss.config.js
+++ wp-content/themes/regulora/resources/assets/build/postcss.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
+/*
 const cssnanoConfig = {
   preset: ['default', { discardComments: { removeAll: true } }]
 };
@@ -13,3 +14,27 @@ module.exports = ({ file, options }) => {
     },
   };
 };
+*/
+
+module.exports = {
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+        'postcss-flexbugs-fixes',
+        [
+          'postcss-preset-env',
+          {
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+            features: {
+              'custom-properties': false,
+            },
+          },
+        ],
+      ]
+      : [
+        // No transformations in development
+      ],
+}
