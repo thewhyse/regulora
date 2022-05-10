3bdcaa28d44fd6f318c792b87e946c5f5767ed95
Max <maxsevostianov@192.168.1.3>
2022-05-10 16:38:23 -0400

    added target=_blank to footer menu links

--- wp-content/themes/regulora/resources/assets/scripts/customizer.js
+++ wp-content/themes/regulora/resources/assets/scripts/customizer.js
@@ -3,3 +3,9 @@ import $ from 'jquery';
 wp.customize('blogname', (value) => {
   value.bind(to => $('.brand').text(to));
 });
+
+
+const blank = $('#menu-footer-menu-1 li a');
+blank.each(function () {
+  $(this).attr('target', '_blank');
+});
