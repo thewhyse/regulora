b36ff9162d727b27f23d7cb5452952600cf26e46
Max <maxsevostianov@192.168.1.3>
2022-10-05 15:12:07 -0400

    footer logo + styles

--- wp-content/themes/regulora_new/resources/views/partials/footer.blade.php
+++ wp-content/themes/regulora_new/resources/views/partials/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="page-footer content-info">
   <div class="container">
-    <div class="alignwide">
+    {{--<div class="alignwide">--}}
+    <div class="footer-holder">
       <div class="row mx-0 align-items-center">
         <div class="col-12 col-lg-1 p-0 text-center">
           <a class="footer-logo" href="{{ home_url('/') }}" aria-label="Footer Logo">
