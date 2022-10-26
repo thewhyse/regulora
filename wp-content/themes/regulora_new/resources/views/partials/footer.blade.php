7bbc20e98b85efde7cf109b5dd004641ba6d7ddb
Max <maxsevostianov@192.168.1.3>
2022-10-26 17:22:34 -0400

    styles clean up 10

--- wp-content/themes/regulora_new/resources/views/partials/footer.blade.php
+++ wp-content/themes/regulora_new/resources/views/partials/footer.blade.php
@@ -31,10 +31,10 @@
           <div class="col-12 col-lg-3">
             @php dynamic_sidebar('sidebar-footer-left') @endphp
           </div>
-          <div class="col-12 col-lg-5">
+          <div class="col-12 col-lg-6">
             @php dynamic_sidebar('sidebar-footer-center') @endphp
           </div>
-          <div class="col-12 col-lg-4">
+          <div class="col-12 col-lg-3">
             @php dynamic_sidebar('sidebar-footer-right') @endphp
             <div class="copyright">
               {!! App::siteFooterCopyright() !!}
