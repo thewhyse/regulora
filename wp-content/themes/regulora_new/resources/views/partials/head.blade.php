298005090c9a23716555e72b4a79290f1689cd25
Max <maxsevostianov@192.168.1.3>
2022-11-13 17:45:17 -0500

    added link rel='preload' 2

--- wp-content/themes/regulora_new/resources/views/partials/head.blade.php
+++ wp-content/themes/regulora_new/resources/views/partials/head.blade.php
@@ -3,7 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="preload" as="style" href="/wp-content/themes/regulora_new/dist/scripts/main_'\*'.js">
+  <link rel="preload" as="style" href="/wp-content/themes/regulora_new/dist/scripts/main_*.js">
 
   @php wp_head() @endphp
 </head>
