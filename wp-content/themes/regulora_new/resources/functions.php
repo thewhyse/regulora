1ab888b2e88c1cc5fa8253b93c8cab0e6adc5cd1
Max <maxsevostianov@192.168.1.3>
2022-11-13 20:27:48 -0500

    stylesheets thing 4

--- wp-content/themes/regulora_new/resources/functions.php
+++ wp-content/themes/regulora_new/resources/functions.php
@@ -95,7 +95,7 @@ Container::getInstance()
 function regulora_patient_site_assets() {
 	$pat =  preg_match("/[^A-Za-z0-9]+/");
 	wp_register_style( 'reg_patient_site_stylesheet', get_theme_file_uri() . '/dist/styles/main_' . $pat . '.css', array(), '1.0.0', 'all');
-	wp_enqueue_style('reg_patient_site_styleshe', false);
+	wp_enqueue_style('reg_patient_site_styleshe');
 	/*wp_enqueue_script('reg_patient_site_js', get_theme_file_uri() . '/dist/js/bundle.js', array('jquery'), '1.0.0', true);*/
 }
 add_action('wp_enqueue_scripts', 'regulora_patient_site_assets');
