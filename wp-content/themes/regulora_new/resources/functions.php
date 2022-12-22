ac7a93022e3488d44c98b18a442c72cb36c45501
Max <maxsevostianov@192.168.1.3>
2022-12-22 16:39:47 -0500

    added links to gsap

--- wp-content/themes/regulora_new/resources/functions.php
+++ wp-content/themes/regulora_new/resources/functions.php
@@ -90,3 +90,16 @@ Container::getInstance()
             'view' => require dirname(__DIR__).'/config/view.php',
         ]);
     }, true);
+
+// The proper way to enqueue GSAP script in WordPress
+
+// wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+function theme_gsap_script(){
+	// The core GSAP library
+	wp_enqueue_script( 'gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js', array(), false, true );
+	// ScrollTrigger - with gsap.js passed as a dependency
+	wp_enqueue_script( 'gsap-st', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js', array('gsap-js'), false, true );
+	// Your animation code file - with gsap.js passed as a dependency
+	wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . 'scripts/gsap-app.js', array('gsap-js'), false, true );
+}
+add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
