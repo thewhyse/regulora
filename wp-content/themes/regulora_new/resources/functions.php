9e56673d04e04a9a8723258523dbd0904db1b133
Max <maxsevostianov@192.168.1.3>
2022-12-22 17:31:43 -0500

    gsap js via functions.

--- wp-content/themes/regulora_new/resources/functions.php
+++ wp-content/themes/regulora_new/resources/functions.php
@@ -100,6 +100,6 @@ function theme_gsap_script(){
 	// ScrollTrigger - with gsap.js passed as a dependency
 	wp_enqueue_script( 'gsap-st', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js', array('gsap-js'), false, true );
 	// Your animation code file - with gsap.js passed as a dependency
-	wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . 'scripts/gsap-app.js', array('gsap-js'), false, true );
+	wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . 'assets/scripts/gsap-app.js', array('gsap-js'), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
