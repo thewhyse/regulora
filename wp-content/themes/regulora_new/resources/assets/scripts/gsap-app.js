4c7b49f93fe8de1ca17848a8ae6332f4eb45c994
Max <maxsevostianov@192.168.1.3>
2022-12-28 16:56:59 -0500

    responcive pain anim

--- wp-content/themes/regulora_new/resources/assets/scripts/gsap-app.js
+++ wp-content/themes/regulora_new/resources/assets/scripts/gsap-app.js
@@ -57,10 +57,44 @@ document.addEventListener('DOMContentLoaded', function(event){
 });
 
 
-const kartinka = document.querySelector('.dt-pain img');
 window.addEventListener('resize', function(event) {
-  let imageLeft = kartinka.getBoundingClientRect().left;
-  let imageRight = kartinka.getBoundingClientRect().right;
 
-  console.log(imageLeft + ' = imageLeft' , imageRight + ' =imageRight');
+  gsap.registerPlugin(ScrollTrigger);
+
+  const img = document.querySelector('.dt-pain img'),
+    bow = document.querySelector('.dt-pain .bow'),
+    aft = document.querySelector('.dt-pain .aft'),
+    downw = document.querySelector('.dt-pain .downw');
+
+  let imageLeft = img.getBoundingClientRect().left;
+  let imageRight = img.getBoundingClientRect().right;
+
+  let ww = window.innerWidth;
+
+  let stopPoint = ww - (ww - imageLeft);
+
+  const pain_container = document.querySelector('.dt-pain');
+  let aft_width = (pain_container.offsetWidth - 197) / 2;
+
+  const pain_column = document.querySelector('#pain-column');
+  let half_column = pain_column.offsetHeight / 2;
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.dt-pain',
+      markers: true,
+      start: 'top 60%',
+    },
+
+  });
+
+  tl.to(bow, {x: imageLeft, duration: 1})
+    .to(aft, {right: aft_width, x: aft_width + 10, width: aft_width + 10, duration: 1})
+    .to(downw, {
+      //right: - aft_width,
+      bottom: - (half_column - 11),
+      height: (half_column -5),
+      duration: 2,
+    });
+
 }, true);
