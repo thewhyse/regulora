1876515d5ad2ba38d46060d84b6b1bec0086040d
Max <maxsevostianov@192.168.1.3>
2023-01-01 17:37:24 -0500

    bgc page anime 8

--- wp-content/themes/regulora_new/resources/assets/scripts/gsap-app.js
+++ wp-content/themes/regulora_new/resources/assets/scripts/gsap-app.js
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', function(event){
       .to(right_line, {
        // top: - imgContainerBox.top,
         x: - (imgContainerBox.width - imgContainerBox.left) / 2,
-        width: (imgContainerBox.width - imgContainerBox.left) / 2,
+        width: (imgContainerBox.width - imgContainerBox.left),
         duration: 1,
       });
 
