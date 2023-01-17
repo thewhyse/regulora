0812675cca1928c52168199d24e25f427f154eb9
Max <maxsevostianov@192.168.1.3>
2023-01-17 17:37:51 -0500

    bgc anim js 3

--- wp-content/themes/regulora_new/resources/assets/scripts/gsap-app.js
+++ wp-content/themes/regulora_new/resources/assets/scripts/gsap-app.js
@@ -143,7 +143,8 @@ console.log(leftSide);
     const percentColumn = document.querySelector('.restoring-left-side');
     let rightLineW = percentColumn.offsetWidth / 4;
 
-    leftL.style.left -= + leftSide + 'px';
+    let windowWOffSet = window.innerWidth - (window.innerWidth - leftSide);
+    leftL.style.left -= + windowWOffSet + 'px';
 
     const tl_bgc_desktop = gsap.timeline({
       scrollTrigger: {
